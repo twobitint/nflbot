75a9d2e394369c1dfbcafe8caa5501b2d374dcc9
Justin Labak <jtlabak@uw.edu>
2016-07-28 15:34:49 -0700

    command regex

--- slack_bot.js
+++ slack_bot.js
@@ -57,59 +57,63 @@ client.connect(function (err) {
     if (err) throw err;
     client.query('select * from player limit 1', function (err, result) {
         if (err) throw err;
-        console.log(prettyjson.render(result));
+        //console.log(prettyjson.render(result));
         client.end(function (err) {
             if (err) throw err;
         });
     });
 });
 
+//Run a simple web server for slack commands
+controller.setupWebserver(process.env.SERVER_PORT, function (err, webserver) {
+    controller.createWebhookEndpoints(controller.webserver);
+});
 
+controller.on('slash_command', function (bot, message) {
+    console.log(message);
+    switch (message.command) {
+        case '/nflbot':
+            nflCommand(bot, message);
+            break;
+        default:
+            bot.replyPrivate(message, 'Your command is not allowed');
+    }
+});
 
+function nflCommand(bot, message) {
+    if (message.token == process.env.SLACK_CMD_NFLBOT) {
+        var found = message.text.match(/(.*) (schedule|stats)( ([\d]*))?/i);
+        if (found) {
+            var name = found[1];
+            var type = found[2];
+            var year = found[4];
+        }
+        // bgg('/search', {query: message.text, type: 'boardgame'}, function (res) {
+        //     var results = res.items.item;
+        //
+        //     if (results.length == 0) {
+        //         bot.replyPrivate(message, 'No results found for “'+message.text+'”.');
+        //     }
+        //     var matches = results.filter(function (elem) {
+        //         return elem.name.value.toUpperCase() == message.text.toUpperCase();
+        //     });
+        //     if (matches.length == 0) {
+        //         matches = results;
+        //     }
+        //
+        //     var year = -99999;
+        //     var match = null;
+        //     matches.forEach(function (elem) {
+        //         if (elem.yearpublished.value > year) {
+        //             match = elem;
+        //             year = elem.yearpublished.value;
+        //         }
+        //     });
+        //     replyGame(bot, message, match.id);
+        // });
+    }
+}
 
-// Run a simple web server for slack commands
-// controller.setupWebserver(process.env.SERVER_PORT, function (err, webserver) {
-//     controller.createWebhookEndpoints(controller.webserver);
-// });
-//
-// controller.on('slash_command', function (bot, message) {
-//     switch (message.command) {
-//         case '/bgg':
-//             gameCommand(bot, message);
-//             break;
-//         default:
-//             bot.replyPrivate(message, 'Your command is not allowed');
-//     }
-// });
-//
-// function gameCommand(bot, message) {
-//     if (message.token == process.env.SLACK_BGG_CMD_TOKEN) {
-//         bgg('/search', {query: message.text, type: 'boardgame'}, function (res) {
-//             var results = res.items.item;
-//
-//             if (results.length == 0) {
-//                 bot.replyPrivate(message, 'No results found for “'+message.text+'”.');
-//             }
-//             var matches = results.filter(function (elem) {
-//                 return elem.name.value.toUpperCase() == message.text.toUpperCase();
-//             });
-//             if (matches.length == 0) {
-//                 matches = results;
-//             }
-//
-//             var year = -99999;
-//             var match = null;
-//             matches.forEach(function (elem) {
-//                 if (elem.yearpublished.value > year) {
-//                     match = elem;
-//                     year = elem.yearpublished.value;
-//                 }
-//             });
-//             replyGame(bot, message, match.id);
-//         });
-//     }
-// }
-//
 // function bgg(path, options, callback) {
 //     var api = 'https://www.boardgamegeek.com/xmlapi2';
 //     request({
@@ -121,50 +125,50 @@ client.connect(function (err) {
 //         }
 //     });
 // }
-//
-// function replyGame(bot, message, gameId) {
-//     var options = {
-//         id: gameId,
-//         stats: 1
-//     };
-//     bgg('/thing', options, function (res) {
-//         var info = res.items.item;
-//         // var year = info.release_date.substring(0, 4);
-//         // var thumb = configuration.images.base_url
-//         //     + configuration.images.poster_sizes[0]
-//         //     + info.poster_path;
-//         // var release = moment(info.release_date).format('dddd, MMMM Do YYYY');
-//         bot.replyPublicDelayed(message, {
-//             text: 'This is what I found for “' + message.text + '”',
-//             attachments: [
-//                 {
-//                     title: info.name[0].value + ' (' + info.yearpublished.value + ')',
-//                     title_link: 'https://boardgamegeek.com/boardgame/'+info.id,
-//                     thumb_url: info.thumbnail.replace('//', 'https://'),
-//                     text: info.description,
-//                     fields: [
-//                         {
-//                             title: 'Rating',
-//                             value: info.statistics.ratings.ranks.rank[0].bayesaverage,
-//                             short: true
-//                         },
-//                         {
-//                             title: 'Playing Time',
-//                             value: (info.playingtime.value) + ' minutes',
-//                             short: true
-//                         },
-//                         {
-//                             title: 'Core Mechanics',
-//                             value: info.link.filter(function (cur) {
-//                                 return cur.type == 'boardgamemechanic';
-//                             }).map(function (cur) {
-//                                 return cur.value;
-//                             }).join(', '),
-//                             short: true
-//                         }
-//                     ]
-//                 }
-//             ]
-//         });
-//     });
-// }
+
+function replyGame(bot, message, gameId) {
+    var options = {
+        id: gameId,
+        stats: 1
+    };
+    bgg('/thing', options, function (res) {
+        var info = res.items.item;
+        // var year = info.release_date.substring(0, 4);
+        // var thumb = configuration.images.base_url
+        //     + configuration.images.poster_sizes[0]
+        //     + info.poster_path;
+        // var release = moment(info.release_date).format('dddd, MMMM Do YYYY');
+        bot.replyPublicDelayed(message, {
+            text: 'This is what I found for “' + message.text + '”',
+            attachments: [
+                {
+                    title: info.name[0].value + ' (' + info.yearpublished.value + ')',
+                    title_link: 'https://boardgamegeek.com/boardgame/'+info.id,
+                    thumb_url: info.thumbnail.replace('//', 'https://'),
+                    text: info.description,
+                    fields: [
+                        {
+                            title: 'Rating',
+                            value: info.statistics.ratings.ranks.rank[0].bayesaverage,
+                            short: true
+                        },
+                        {
+                            title: 'Playing Time',
+                            value: (info.playingtime.value) + ' minutes',
+                            short: true
+                        },
+                        {
+                            title: 'Core Mechanics',
+                            value: info.link.filter(function (cur) {
+                                return cur.type == 'boardgamemechanic';
+                            }).map(function (cur) {
+                                return cur.value;
+                            }).join(', '),
+                            short: true
+                        }
+                    ]
+                }
+            ]
+        });
+    });
+}
